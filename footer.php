98b9d2e756c9cb97a83ee3f2550f0c03cfe8a8a5
Jeremy Boggs <jeremy@clioweb.org>
2011-05-02 08:46:15 -0400

    Adds RRCHNM logo to theme footer.

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 		<div id="footer">
           
           	<div id="chnm-meta">
-          	    <a href="http://chnm.gmu.edu" id="chnm-logo"><img alt="Center for History and New Media" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/chnm-logo-regular.png" /></a>
+          	    <a href="http://chnm.gmu.edu" id="chnm-logo"><img alt="Center for History and New Media" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/rrchnm-logo-regular.gif" /></a>
           	    <p id="copyright-info"><a href="<?php bloginfo('home'); ?>"><?php bloginfo('title'); ?></a> is a project of the <a href="http://chnm.gmu.edu">Center for History and New Media</a>, <a href="http://www.gmu.edu">George Mason University</a>. Generously funded by the National Endowment for the Humanities. &copy; 2010, Center for History and New Media.</p>
 
 
